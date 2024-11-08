3faf7c76482f2a610b63e5bfd86d56075e46ee87
SpiffGreen <spiffjekeygreen@gmail.com>
2024-11-08 14:32:31 +0100

    feat: initial commit

---
+++ src/app/server.ts
@@ -0,0 +1,14 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import serverless from 'serverless-http';
+
+const app: Application = express();
+
+// @ts-expect-error
+app.get('/api/ping', (_req: Request, res: Response) => {
+  return res.json({
+    success: true,
+    message: 'Endpoint is working',
+  });
+});
+
+export const handler = serverless(app, { provider: 'aws' });
